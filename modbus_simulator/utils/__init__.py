5b88143faea37fdd3bce51aadb942b89a0510471
sanjay <sanjay@riptideio.com>
2017-09-22 09:19:54 +0530

    minor updates

--- modbus_simulator/utils/__init__.py
+++ modbus_simulator/utils/__init__.py
@@ -1,5 +1,1 @@
-"""
-Copyright (c) 2016 Riptide IO, Inc. All Rights Reserved.
-
-"""
 from __future__ import absolute_import, unicode_literals
--- modbus_simulator/utils/pymodbus_server.py
+++ modbus_simulator/utils/pymodbus_server.py
@@ -1,7 +1,3 @@
-"""
-Copyright (c) 2017 Riptide IO, Inc. All Rights Reserved.
-
-"""
 from __future__ import absolute_import, unicode_literals
 
 from pymodbus.server.sync import ModbusSerialServer
